79622202692fd95141a27b9632296cd1262e3c21
Kushwanth12215773 <119439255+Kushwanth-004@users.noreply.github.com>
2025-07-12 23:26:24 +0530

    deploy

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,9 @@
 // export const BASE_URL = "/api";
-export const BASE_URL = "http://localhost:8888";
+// export const BASE_URL = "http://localhost:8888";
 
 // export const BASE_URL = "https://devtinder-backend-pa1s.onrender.com"
+
+// constant.js
+export const BASE_URL = import.meta.env.PROD
+  ? "https://devtinder-backend-pa1s.onrender.com"
+  : "http://localhost:8888";
--- src/utils/socket.js
+++ src/utils/socket.js
@@ -5,6 +5,11 @@ export const createSocketConnection = () => {
   if (location.hostname === "localhost") {
     return io(BASE_URL);
   } else {
-    return io("/", { path: "/api/socket.io" });
+    // return io("/", { path: "/api/socket.io" });
+    return io("https://your-backend.onrender.com", {
+      path: "/socket.io", // or "/api/socket.io" if you've set it
+      transports: ["websocket"],
+      withCredentials: true,
+    });
   }
 };
