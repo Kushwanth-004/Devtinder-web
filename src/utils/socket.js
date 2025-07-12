55a5ee74659342fd79ec679a83d6db4eb4002647
Kushwanth12215773 <119439255+Kushwanth-004@users.noreply.github.com>
2025-07-13 01:21:15 +0530

    deploy

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,13 +1,30 @@
+// import { io } from "socket.io-client";
+// import { BASE_URL } from "./constant";
+
+// export const createSocketConnection = () => {
+//   if (location.hostname === "localhost") {
+//     return io(BASE_URL);
+//   } else {
+//     // return io("/", { path: "/api/socket.io" });
+//     return io("https://devtinder-backend-pa1s.onrender.com", {
+//       path: "/socket.io", // or "/api/socket.io" if you've set it
+//       transports: ["websocket"],
+//       withCredentials: true,
+//     });
+//   }
+// };
+
 import { io } from "socket.io-client";
 import { BASE_URL } from "./constant";
 
 export const createSocketConnection = () => {
   if (location.hostname === "localhost") {
-    return io(BASE_URL);
+    return io(BASE_URL, {
+      transports: ["websocket"],
+      withCredentials: true,
+    });
   } else {
-    // return io("/", { path: "/api/socket.io" });
     return io("https://devtinder-backend-pa1s.onrender.com", {
-      path: "/socket.io", // or "/api/socket.io" if you've set it
       transports: ["websocket"],
       withCredentials: true,
     });
